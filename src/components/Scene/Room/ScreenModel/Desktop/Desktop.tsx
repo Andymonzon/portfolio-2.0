289f865871008185e52f84e68c17d733783c3897
Andymonzon <valentinomonzonandres@gmail.com>
2025-07-01 21:38:52 -0300

    agrego carpeta juegos

--- src/components/Scene/Room/ScreenModel/Desktop/Desktop.tsx
+++ src/components/Scene/Room/ScreenModel/Desktop/Desktop.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Browser } from "../Browser/Browser";
 import type { AppNames, AppState } from "../ScreenHtml/ScreenHtml";
 
+import WindowsFolder from "/screen/desktop/folder.png";
+
 interface Props {
   setIsMinimized: React.Dispatch<
     React.SetStateAction<Record<AppNames, AppState>>
@@ -21,7 +23,7 @@ export const Desktop = ({ setIsMinimized, isMinimized }: Props) => {
 
   return (
     <div
-      className="w-full flex flex-grow rounded-t-sm box-border"
+      className="w-full flex flex-grow rounded-t-sm box-border relative"
       style={{
         backgroundImage: 'url("https://i.gifer.com/g32K.gif")',
         backgroundSize: "cover",
@@ -33,6 +35,10 @@ export const Desktop = ({ setIsMinimized, isMinimized }: Props) => {
       {isMinimized.brave.value ? null : (
         <Browser handleMinimize={handleMinimize} />
       )}
+      {/* <div className="absolute top-3 left-3 flex items-center justify-center flex-col select-none cursor-pointer">
+        <img src={WindowsFolder} alt="folder" width={50} height={50} />
+        <span>Juegos</span>
+      </div> */}
     </div>
   );
 };
